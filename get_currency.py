79486cf2cda23c9c99de7288c46eb7de10c9fe8d
pedrobaf <pedrobaf@gmail.com>
2022-12-20 19:50:56 +0400

    update commit

--- get_currency.py
+++ get_currency.py
@@ -1,13 +1,13 @@
 import requests
 import os 
 import json
-import csv
 import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv() 
 
 API_KEY = os.getenv("API_KEY")
+basePath = os.path.dirname(os.path.abspath(__file__))
 base = "USD"
 symbols = "BRL"
 
@@ -21,6 +21,7 @@ with open("res.json", "w") as f:
     f.write(json.dumps(response.json(), indent=4)) 
     
 # converting json to csv file
-df = pd.read_json (r'/Users/pedro/Desktop/Projects/res.json')
-df.to_csv (r'/Users/pedro/Desktop/Projects/res.csv', index = None)
+df = pd.read_json ("res.json")
+df.to_csv (basePath + "/res.csv", index = None)
+
 
